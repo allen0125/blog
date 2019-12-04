708f9e3b98c5745e157b35f24431b2baca167d2d
Allen Wong <wangyao0125@gmail.com>
2019-12-04 20:46:06 +0800

    完成View修改为ClassView

--- article/urls.py
+++ article/urls.py
@@ -5,10 +5,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    # url(r'^blog', views.home, name='home'),
     url(r'^blog', views.PostListView.as_view(), name='blog_list'),
-    # url(r'^(\d+)/', views.detail, name='detail'),
     path(r'<post_id>/', views.PostDetailView.as_view(), name='blog_detail'),
-    # path('todolist', views.TodoDetailView.as_view(), name='todolist'),
-    # path('about', views.AllenCVView.as_view(), name='about'),
+    path('todolist', views.TodoDetailView.as_view(), name='todolist'),
+    path('about', views.AllenCVView.as_view(), name='about'),
 ]

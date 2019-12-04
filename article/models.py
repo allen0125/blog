4a517efb7e06434590cad707d449ed421f9cc091
Allen Wong <wangyao0125@gmail.com>
2019-12-04 23:55:58 +0800

    更新模型

--- article/models.py
+++ article/models.py
@@ -6,6 +6,7 @@ class Article(models.Model):
     title = models.CharField(max_length = 100)  # 博客题目
     is_bio = models.BooleanField(default=False)  # 是否是bio
     is_blog = models.BooleanField(default=True) # 是否为博客文章
+    is_todolist = models.BooleanField(default=False)
     category = models.CharField(max_length = 50, blank=True)  # 博客标签
     date_time = models.DateTimeField(auto_now_add=True)  # 博客日期
     content = MarkdownxField(default='') # 博客文章正文
@@ -26,7 +27,12 @@ class Article(models.Model):
         queryset = cls.objects.filter(is_blog=True)
         return queryset
 
-    @classmethod
-    def get_bio(cls):
-        queryset = cls.objects.get(is_bio=True)
-        return queryset
+    # @classmethod
+    # def get_bio(cls):
+    #     queryset = cls.objects.get(is_bio=True)
+    #     return queryset
+
+    # @classmethod
+    # def get_todolist(cls):
+    #     queryset = cls.objects.get(is_todolist=True)
+    #     return queryset
